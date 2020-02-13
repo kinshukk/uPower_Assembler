08f592030363de729bc224e2066e532ae630bd06
JayFaldu <jdf.181co101@nitk.edu.int>
2020-02-13 23:00:13 +0530

    tokenize into instruction,directives and label wq

--- src/Tokenizer.py
+++ src/Tokenizer.py
@@ -1,6 +1,21 @@
+
 class Tokenizer:
     def __init__(self):
-        pass
+        self.assembler_direc=[]
+        self.instruct=[]
+        self.label=[]
 
     def run(self, source):
-        return source
+        return self.line_detect(source)
+
+    def line_detect(self,lines):
+        for idx,li in enumerate(lines):
+            tmp=li.split(" ")
+            if tmp[0]==".data":
+                self.assembler_direc.append((tmp,idx))
+            elif tmp[0]==".text":
+                self.assembler_direc.append((tmp,idx))
+            elif tmp[0].endswith(":"):
+                self.label.append((tmp,idx))
+            else:
+                self.instruct.append((tmp,idx))
