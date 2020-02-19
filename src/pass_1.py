5dc34358b1e22168dd05548b8f420f00754e2791
Kinshuk Kashyap <kinshuk.other@gmail.com>
2020-02-19 19:48:55 +0530

    assembly

--- src/pass_1.py
+++ src/pass_1.py
@@ -1,73 +1,59 @@
 import re
 
 def get_symbol_table_instructions(lines):
-	f = lines
-	#print(f)
-	start_data=0
-	start_text=0
-	for i in range(len(f)):
-		if f[i]==".text":
-			start_text=i
-			#print(start_text)
-		if f[i]==".data":
-			start_data=i
-			#print(start_data)
+        f = lines
+        start_data=0
+        start_text=0
+        for i in range(len(f)):
+                if f[i]==".text":
+                        start_text=i
+                if f[i]==".data":
+                        start_data=i
 
 
-	cur_location=0x4000000
-	text={}
-	instruction={}
-	var=re.compile(r'.+:')
-	for i in range(start_text+1,len(f)):
-		#print(hex(cur_location))
-		if bool(var.match(f[i])):
-			x=re.split(":",f[i])
-			text[x[0]]=hex(cur_location)
-			cur_location=cur_location-4
-		else:
-			tmp=f[i].split('#')[0]
-			tmp=" ".join(tmp.split())
-			instruction[str(hex(cur_location))]=tmp	
-		cur_location=cur_location+4
+        cur_location=0x4000000
+        text={}
+        instruction={}
+        var=re.compile(r'.+:')
+        for i in range(start_text+1,len(f)):
+                if bool(var.match(f[i])):
+                        x=re.split(":",f[i])
+                        text[x[0]]=hex(cur_location)
+                        cur_location=cur_location-4
+                else:
+                        tmp=f[i].split('#')[0]
+                        tmp=" ".join(tmp.split())
+                        instruction[str(hex(cur_location))]=tmp 
+                cur_location=cur_location+4
 
 
-	# print(instruction)
-	# print(text)
-	cur_location=0x10000000
-	data={}
-	for i in range(start_data+1,start_text):
-		if bool(var.match(f[i])):
-			arr=f[i].split()
-			lable=re.split(":",arr[0])[0]
-			data[lable]=hex(cur_location)
-			datatype=arr[1][1:]
-			if datatype=="byte":
-				cur_location=cur_location+1
-			elif datatype=="word":
-				cur_location=cur_location+4
-			elif datatype=="halfword":
-				cur_location=cur_location+2
-			elif datatype=="space":
-				cur_location=cur_location+int(arr[2])
-			elif datatype=="ascii":
-				flag=0
-				count=0
-				for i in f[i]:
-					if i=="\"" and flag==0:
-						flag=1
-						continue
-					if flag==1 and i!="\"":
-						count=count+1
-					if flag==1 and i=="\"":
-						flag=0
-				cur_location=cur_location+count
-	#print(data)
+        cur_location=0x10000000
+        data={}
+        for i in range(start_data+1,start_text):
+                if bool(var.match(f[i])):
+                        arr=f[i].split()
+                        lable=re.split(":",arr[0])[0]
+                        data[lable]=hex(cur_location)
+                        datatype=arr[1][1:]
+                        if datatype=="byte":
+                                cur_location=cur_location+1
+                        elif datatype=="word":
+                                cur_location=cur_location+4
+                        elif datatype=="halfword":
+                                cur_location=cur_location+2
+                        elif datatype=="space":
+                                cur_location=cur_location+int(arr[2])
+                        elif datatype=="ascii":
+                                flag=0
+                                count=0
+                                for i in f[i]:
+                                        if i=="\"" and flag==0:
+                                                flag=1
+                                                continue
+                                        if flag==1 and i!="\"":
+                                                count=count+1
+                                        if flag==1 and i=="\"":
+                                                flag=0
+                                cur_location=cur_location+count
 
-	return (instruction, text, data)
-
-
-
-
-
-
-
+        return (instruction, text, data)
--- src/pass_22.py
+++ src/pass_22.py
@@ -1,4 +1,3 @@
-
 instruct_x={"and":[31,None,0,28,None,None],"exstw":[31,None,0,986,None,None],"nand":[31,None,0,476,None,None],"or":[31,None,0,444,None,None],"xor":[31,None,0,316,None,None],"sld":[31,None,0,794,None,None],"srd":[31,None,0,539,None,None],"srad":[31,None,0,794,None,None],"cmp":[31,None,0,0,None,None]}
 
 
@@ -12,8 +11,7 @@ ans={"1":"and R1,R3,LR"}
 fin_ans={}
 
 
-for z in ans:
-    
+for z in ans:    
     u=z
     v=ans[z]
     
@@ -21,18 +19,57 @@ for z in ans:
     args=v[v.index(' '):]
     print(func,"asdf ")
     print(args)
+
+    tmp=""
+    req=args.split(",")
+    req=[i.replace(" ","") for i in req]
+
     if func in instruct_x.keys():
+        tmp=tmp+"{:06b}".format(instruct_x[func][0])
+        tmp=tmp+"{:05b}".format(reg_to_num[req[1]])
+        tmp=tmp+"{:05b}".format(reg_to_num[req[0]])
+        tmp=tmp+"{:05b}".format(reg_to_num[req[2]])
+        tmp=tmp+"{:010b}".format(instruct_x[func][3])+"0"
+        tmp=tmp.replace("0b","")
+        fin_ans[u]=tmp
+
+    elif func in instruct_xo.keys():
+        tmp += "{:06b}".format(instruct_xo[func][0])
+        tmp += "{:05b}".format(reg_to_num[req[0]])
+        tmp += "{:05b}".format(reg_to_num[req[1]])
+        tmp += "{:05b}".format(reg_to_num[req[2]])
+        tmp += "{:01b}".format(instruct_xo[func][1])
+        tmp += "{:09b}".format(instruct_xo[func][3]) + "0"
+        tmp = tmp.replace("0b","")
+        fin_ans[u]=tmp
+
+    elif func in instruct_d.keys():
+        if instruct_d[func][-1] == 1:
+            #func RT, D(RA)
+            RT = req[0].trim()
+
+            i1 = req[1].index('(')
+            i2 = req[1].index(')')
+            
+            SI = int(req[1][:i1].trim())
+            RA = req[1][i1+1:i2].trim()
+            
+            tmp += "{:06b}".format(instruct_d[func][0])
+            tmp += "{:05b}".format(reg_to_num[RT])
+            tmp += "{:05b}".format(reg_to_num[RA])
+            tmp += "{:016b}".format(SI)
+            tmp = tmp.replace("0b","")
+            fin_ans[u]=tmp
+
+        elif instruct_d[func][-1] == 0:
+            tmp += "{:06b}".format(instruct_d[func][0])
+            tmp += "{:05b}".format(reg_to_num[req[0]])
+            tmp += "{:05b}".format(reg_to_num[req[1]])
+            tmp += "{:016b}".format(reg_to_num[req[2]])
+            tmp = tmp.replace("0b","")
+            fin_ans[u]=tmp
         
-        tmp_ans=""
-        req=args.split(",")
-        req=[i.replace(" ","") for i in req]
-        
-        tmp_ans=tmp_ans+bin(instruct_x[func][0])
-        tmp_ans=tmp_ans+bin(reg_to_num[req[1]])
-        tmp_ans=tmp_ans+bin(reg_to_num[req[0]])
-        tmp_ans=tmp_ans+bin(reg_to_num[req[2]])
-        tmp_ans=tmp_ans+bin(instruct_x[func][3])+"0"
-        tmp_ans=tmp_ans.replace("0b","");
-        fin_ans[u]=tmp_ans;
+        else:
+            print("\n\nO crap something isn't right. D type assembly\n\n")
 
 print(fin_ans)
